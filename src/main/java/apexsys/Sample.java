7e598bcef71210fabaab06af188a40f36156d1d1
ovidiu.mura <ovidiu.mura@pacteraedge.com>
2022-01-24 16:06:08 -0800

    working with streams in Java 8

---
+++ src/main/java/apexsys/Sample.java
@@ -0,0 +1,15 @@
+package apexsys;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+
+import java.util.*;
+
+public class Sample {
+
+    public static void main(String[] args) {
+        List<Integer> numbers = Arrays.asList(1,2,3,5,4,6,7,8,9,10);
+    }
+
+}
---
+++ src/test/java/apexsys/SampleTest.java
@@ -0,0 +1,12 @@
+package apexsys;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class SampleTest {
+    @Test
+    public void test1() {
+        Sample s = new Sample();
+        Assert.assertTrue(true);
+    }
+}
